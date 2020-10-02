e72206d36c1a9c50caec71e923c5aef66aa4fc1b
AndreyLacerda <andrey-lacerda@hotmail.com>
2020-10-02 13:22:53 -0300

    Lista de eleicoes do usuario finalizada

---
+++ SistemaEleicao/SistemaEleicao/Controllers/ListaEleicaoController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SistemaEleicao.Data.Contexts;
+using SistemaEleicao.Models.Entities;
+
+namespace SistemaEleicao.Controllers
+{
+    public class ListaEleicaoController : Controller
+    {
+
+        private AppDbContext _db;
+
+        public ListaEleicaoController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        [Route("minhas-eleicoes")]
+        public IActionResult MinhasEleicoes()
+        {
+            string idUser = HttpContext.Session.GetString("Id");
+            var eleicoes = _db.Eleicoes
+                                .FromSqlRaw("SELECT * from eleicao where cod_eleicao IN " + 
+                                            "(select cod_eleicao from usuario_x_eleicao where cod_usuario = '" + 
+                                            idUser + "' and organizador = true)");
+            return View(eleicoes);
+        }
+    }
+}
